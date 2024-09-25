a2f141672aa41112dcda59a5566bc983ef686595
guptasanskriti445 <guptasanskriti445@gmail.com>
2024-09-25 13:59:58 +0530

    add jpeg changes

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -10,8 +10,7 @@ declare module '*.png' {
     const value: any;
     export default value;
   }
-  
-  declare module "*.jpeg" {
+      declare module "*.jpeg" {
     const value: string;
     export default value;
   }
